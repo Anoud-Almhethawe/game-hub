f990c4e656b07324ce59025eb07dc64fabbee795
anoud <almhethawe.anoud@gmail.com>
2023-06-01 18:43:52 +0300

    displaying genres

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,20 @@ function App() {
   return (
     <>
       <Grid
-        templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+        templateColumns={{
+          base: "1fr",
+          lg: "200px 1fr",
+        }}
       >
         <GridItem area="nav">
           <Navbar />
         </GridItem>
         <Show above="lg">
-          <GridItem area="aside">
+          <GridItem area="aside" paddingX={2}>
             <GenrasList />
           </GridItem>
         </Show>
